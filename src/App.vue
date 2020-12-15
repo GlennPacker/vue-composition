017ee83ae3161b5dc08682a5bddbdcd1a62fec87
glennpacker <glenn.packer@branthill.co.uk>
2020-12-15 14:23:16 +0000

    composition initial

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CompositionRefs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Refs</h1>
+    <div>Capacity: {{ capacity }}</div>
+    <button @click="increaseCapacity()">Increase Capacity</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from '@vue/composition-api'
+
+export default {
+  setup() {
+    const
+        capacity = ref(3),
+        increaseCapacity = () => {
+            capacity.value++;
+        }
+
+    return { capacity, increaseCapacity };
+  }
+}
+</script>
