c4dc5ef52c5b51ac787ab3f77db6a5d0567d050f
glennpacker <glenn.packer@branthill.co.uk>
2020-12-15 14:23:16 +0000

    composition initial

---
+++ src/components/CompositionFirst.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Capacity: {{ capacity }} ({{ spacesLeft }} spaces)</p>
+    <button @click="increaseCapacity()">Increase Capacity</button>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for="(name, index) in attending" :key="index">
+        {{ name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import { ref, reactive, computed, toRefs } from '@vue/composition-api'
+
+export default {
+  setup() {
+      const event = reactive({
+          capacity: 4,
+          attending: ["Tim", "Bob", "Joe"],
+          spacesLeft: computed(() => {
+            return event.capacity - event.attending.length;
+          })
+        });
+        function increaseCapacity() {
+          event.capacity++;
+        }
+        return { ...toRefs(event), increaseCapacity };
+  }
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- <composition-refs /> -->
+    <composition-first />
+    <composition-second />
+  </div>
+</template>
+
+<script>
+import CompositionRefs from './CompositionRefs';
+import CompositionFirst from './CompositionFirst';
+import CompositionSecond from './CompositionSecond';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+      CompositionRefs,
+      CompositionFirst,
+      CompositionSecond
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
